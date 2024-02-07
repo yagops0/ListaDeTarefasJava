4611fb00851cab464232b6968a7fa9a6777f4809
yagops0 <yagosalvador34@gmail.com>
2024-02-07 12:56:26 -0300

    Uma funcionalidade feita

--- src/main/java/com/DAO/TarefaDAO.java
+++ src/main/java/com/DAO/TarefaDAO.java
@@ -11,7 +11,7 @@ public class TarefaDAO
 {
     public void cadastrarTarefa(Tarefa tarefa)
     {
-        String sql = "INSERT INTO tarefas VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO tarefas(descricao, prioridade, status, dataConclusao) VALUES (?, ?, ?, ?)";
 
         PreparedStatement ps = null;
 
@@ -19,12 +19,14 @@ public class TarefaDAO
         {
             ps = Conexao.getConexao().prepareStatement(sql);
 
+            //ps.setInt(1, tarefa.getId());
             ps.setString(1, tarefa.getDescricao());
             ps.setInt(2, tarefa.getPrioridade());
             ps.setInt(3, tarefa.getStatus());
             ps.setDate(4, new java.sql.Date(tarefa.getDataConclusao().getTime()));
 
             ps.execute();
+
             ps.close();
            
         }catch(SQLException e)
--- src/main/java/com/yagos/ListaDeTarefas.java
+++ src/main/java/com/yagos/ListaDeTarefas.java
@@ -11,19 +11,50 @@ import com.classes.Tarefa;
  * Hello world!
  *
  */
+
+
 public class ListaDeTarefas 
 {
     public static void main( String[] args )
     {
         Tarefa t = new Tarefa();
+        ListaDeTarefas ldt = new ListaDeTarefas();
         SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd");
-        t.setDescricao("Jogar lixo");
-        t.setPrioridade(1);
+        Date data = ldt.formatarData("2024/03/28");
+
+        
+        //t.setId(1);
+        //t.setDescricao("Jogar lixo");
+        //t.setPrioridade(1);
+        //t.setStatus(2); 
+        //t.setDataConclusao(data);
+
+        t.setDescricao("Estudar");
+        t.setPrioridade(0);
         t.setStatus(2);
+        t.setDataConclusao(data);
+
         
        
         new TarefaDAO().cadastrarTarefa(t);
         
 
     }
+
+    private Date formatarData(String data)
+    {
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd");
+        try 
+        {
+            Date data1 = formato.parse(data);
+            return data1;
+
+        } 
+        catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
+
+
